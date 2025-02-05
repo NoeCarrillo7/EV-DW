897697c8e29027e9dc245914c769459546a48681
NoeCarrillo7 <noelizardi7@gmail.com>
2025-02-05 01:46:08 -0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,82 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+interface Contacto {
+  nombre: string;
+  telefono: string;
+  favorito: boolean;
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'parcial1';
+  title = 'Agenda de Contactos';
+
+  // Lista de contactos
+  contactos: Contacto[] = [];
+  enviado = false;
+  modoEdicion = false;
+  indexEdicion: number | null = null;
+
+  // Formulario Reactivo
+  formContacto = new FormGroup({
+    nombre: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3)
+    ]),
+    telefono: new FormControl('', [
+      Validators.required,
+      Validators.pattern('^[0-9]{10}$') // Solo números de 10 dígitos
+    ]),
+    favorito: new FormControl(false)
+  });
+
+  // Método para agregar un contacto
+  onSubmit() {
+    this.enviado = true;
+
+    if (this.formContacto.valid) {
+      this.contactos.push(this.formContacto.value as Contacto);
+      this.resetFormulario();
+    }
+  }
+
+  // Seleccionar contacto
+  seleccionarContacto(contacto: Contacto, index: number) {
+    this.formContacto.setValue({
+      nombre: contacto.nombre,
+      telefono: contacto.telefono,
+      favorito: contacto.favorito
+    });
+
+    this.modoEdicion = true;
+    this.indexEdicion = index;
+  }
+
+  // Modificar contacto
+  modificarContacto() {
+    if (this.indexEdicion !== null && this.formContacto.valid) {
+      this.contactos[this.indexEdicion] = this.formContacto.value as Contacto;
+      this.resetFormulario();
+      this.modoEdicion = false;
+      this.indexEdicion = null;
+    }
+  }
+
+  // Eliminar contacto
+  eliminarContacto(index: number) {
+    this.contactos.splice(index, 1);
+  }
+
+  // Reiniciar formulario
+  resetFormulario() {
+    this.formContacto.reset();
+    this.enviado = false;
+  }
 }
